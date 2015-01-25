b811c8aec95498e16e6d301e1b860835e9b74f28
Ivan Cheung <veloscape.dev@gmail.com>
2015-01-25 16:00:57 +1100

    corrected accordion spelling

--- app/views/map/accordian.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-
-    @include('map.accordian-item', array('title' => "Collapsible Group Item #3", 'id' => "Three"))
-
-</div>  <!-- end of accordion -->
-
