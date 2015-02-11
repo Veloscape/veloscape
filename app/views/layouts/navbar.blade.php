068b6d07b1ca00599794d12187b9fec886199f9e
Ivan Cheung <veloscape.dev@gmail.com>
2015-02-11 15:12:36 +1100

    set responsive container bounds on different media sizes

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="navbar navbar-inverse navbar-static-top">
-    <div class="container-fluid">
+    <div class="container">
         <div class="navbar-header">
             <a class="navbar-brand" href="#">
                 <span class="logo"></span>
