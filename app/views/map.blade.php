be63de9562b2d7399df4634fda2498e591615b04
Ivan Cheung <veloscape.dev@gmail.com>
2015-02-12 15:31:24 +1100

    modified css container bounds to be more seemless

--- app/views/map.blade.php
+++ app/views/map.blade.php
@@ -29,7 +29,7 @@
 @section('body')
     
     @include('layouts.navbar')
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
             <form>
                 <div class="col-sm-5 col-md-offset-3 form-group">
