bf5a3260f98536433bb9cf1a844b18085b77b0db
Ivan Cheung <veloscape.dev@gmail.com>
2015-02-13 11:34:39 +1100

    removed old test files / relocated deprec test file

--- app/views/test.blade.php
+++ app/views/test.blade.php
@@ -2,28 +2,41 @@
 
 @section('head')
     @parent
+    <script src="http://maps.googleapis.com/maps/api/js"></script>
+    @include('js.map')
+    @include('js.map-style')
+    @include('js.map-events')
+    <script>
+        $(document).ready(function() {
+            $('#feedback-container').height($('#map').height());
+        });
+        
+        $(window).resize(function() {
+            $('#feedback-container').height($('#map').height());
+            $('#accordion').accordion("refresh");
+        });
+
+        $(function() {
+            $( "#accordion" ).accordion({
+                heightStyle: "fill",
+                collapsible: "true"
+            });
+        });
     
- <script>
-  $(function() {
-      $( "#accordion" ).accordion({
-          heightStyle: "fill",
-            collapsible: "true"
-      });
-  });
-  </script>
-      @stop
+    </script>
+    @stop
 
 @section('body')
+    
+    <div class="container">
+        <div class="row">
 
-
-<div id="accordion">
-    @for ($i = 0; $i < 5; $i++)
-        <h3> title </h3>
-        <div class="row"> 
-        @include('map.form') 
+            <div class="col-md-6" id="map-container" style="background-color: #999999; padding-bottom: 45%;">
+                <button class="btn btn-default" type="button">Create new marker</button>
+            </div>
+            <div class="col-md-6" id="feedback-container" style="background-color: #eeeeee;">
+                @include('map.accordion')
+            </div>
         </div>
-
-    @endfor
-
-</div>
+    </div>
 @stop
