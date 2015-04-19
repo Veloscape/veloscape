cfe8d66a38fa292c16830af5aad473c060f014d7
Ivan Cheung <veloscape.dev@gmail.com>
2015-04-20 02:52:55 +1000

    cleaned up nav a bit more

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -5,40 +5,39 @@
                 <span class="logo"></span>
             </a>
         </div>
-
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li><a href="#">news + events</a></li>
-            <li><a href="#">share your experience</a></li>
-            <li><a href="#">about us</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li class="social-header"><p class="navbar-text">Follow Us</p></li>
-            <li class="social-links"><p class="navbar-text">
-                <a href="http://instagram.com/veloscape_syd" target="_blank">
-                    <span class="fa-stack fa-stack-lg" title="Veloscape Instagram">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-instagram fa-inverse fa-stack-1x"></i>
-                    </span>
-                </a>
-            </li>
-            <li class="social-links"><p class="navbar-text">
-                <a href="https://twitter.com/veloscape_syd" target="_blank">
-                    <span class="fa-stack fa-stack-lg" title="Veloscape Twitter">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-twitter fa-inverse fa-stack-1x"></i>
-                    </span>
-                </a>
-            </li>
-            <li class="social-links"><p class="navbar-text">
-                <a href="https://www.facebook.com/veloscape" target="_blank">
-                    <span class="fa-stack fa-stack-lg" title="Veloscape Facebook">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-facebook fa-inverse fa-stack-1x"></i>
-                    </span>
-                </a>
-            </li>
-        </ul>
+            <ul class="nav navbar-nav">
+                <li><a href="#">news + events</a></li>
+                <li><a href="#">share your experience</a></li>
+                <li><a href="#">about us</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="social-header"><p class="navbar-text">Follow Us</p></li>
+                <li class="social-links"><p class="navbar-text">
+                    <a href="http://instagram.com/veloscape_syd" target="_blank">
+                        <span class="fa-stack fa-stack-lg" title="Veloscape Instagram">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-instagram fa-inverse fa-stack-1x"></i>
+                        </span>
+                    </a>
+                </li>
+                <li class="social-links"><p class="navbar-text">
+                    <a href="https://twitter.com/veloscape_syd" target="_blank">
+                        <span class="fa-stack fa-stack-lg" title="Veloscape Twitter">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-twitter fa-inverse fa-stack-1x"></i>
+                        </span>
+                    </a>
+                </li>
+                <li class="social-links"><p class="navbar-text">
+                    <a href="https://www.facebook.com/veloscape" target="_blank">
+                        <span class="fa-stack fa-stack-lg" title="Veloscape Facebook">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-facebook fa-inverse fa-stack-1x"></i>
+                        </span>
+                    </a>
+                </li>
+            </ul>
         </div> <!-- /.navbar-collapse -->
     </div>
 </div>
