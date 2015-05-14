762cde6f6b95523fc27030636a3da304d7369cba
Ivan Cheung <veloscape.dev@gmail.com>
2015-05-14 21:10:59 +1000

    changes: htaccess , map styles - fixed more htaccess errors - switched google map styles to default (kept places icons disabled)

--- cadence/public/js/map-style.js
+++ cadence/public/js/map-style.js
@@ -5,15 +5,6 @@ author: Ivan Cheung
 */
 var style = [
     {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ece2d9"
-            }
-        ]
-    },
-    {
         "featureType": "poi",
         "elementType": "all",
         "stylers": [
@@ -33,64 +24,10 @@ var style = [
     },
     {
         "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ccdca1"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
         "elementType": "labels.icon",
         "stylers": [
             {
                 "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "hue": "#ff0000"
-            },
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 99
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#808080"
-            },
-            {
-                "lightness": 54
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#767676"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#ffffff"
             }
         ]
     },
@@ -103,19 +40,4 @@ var style = [
             }
         ]
     },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": 43
-            },
-            {
-                "lightness": -11
-            },
-            {
-                "hue": "#0088ff"
-            }
-        ]
-    }
 ]
