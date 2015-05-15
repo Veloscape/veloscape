de26bab8deb88d8adfc17b5236e37bebe8a64dd1
Ivan Cheung <veloscape.dev@gmail.com>
2015-05-15 22:40:27 +1000

    made initial framework for wordpress 404 page

---
+++ wordpress/wp-content/themes/Lora/404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The main template file
+ *
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package Lora
+ */
+
+get_header(); ?>
+
+<div id="wrapper" class="fullscreen">
+
+</div>
+
+<?php get_footer(); ?>
