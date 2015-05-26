b3555af451697c1c27e5f587c915960db5310ee0
Ivan Cheung <veloscape.dev@gmail.com>
2015-05-26 16:41:08 +1000

    added additional start info text; requires dressing up

--- cadence/app/views/map/info.blade.php
+++ cadence/app/views/map/info.blade.php
@@ -1,8 +1,14 @@
 <div class="side-info" style="z-index: 2">
-    <div class="centered">
-        <h1 class="light"> 
-        Place a marker by left-clicking anywhere on the map to begin
-        </h1>
+    <div>
+        <p>
+            Veloscape is an art research project about cycling in Sydney, and we want you to be part of it!</br> 
+            Use this page to share your impressions of your cycling journeys: tell us about a single location, or map an extended route.</br> 
+            You’ll be providing data to the City of Sydney and other councils to help them improve their cycling infrastructure. </br>
+            You’ll also be helping us to create public artworks that explore how Sydney is experienced by bike. 
+        </p>
+        <p>
+            Left-click anywhere on the map to begin. Click on the <i class="fa fa-question"></i> if you require help.
+        </p>
     </div>
 </div>
 
