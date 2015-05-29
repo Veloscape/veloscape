b181d2c15228cff2624eb739a5cec0842d3b79f4
Ivan Cheung <veloscape.dev@gmail.com>
2015-05-29 17:22:56 +1000

    contact form js and php changes (hopefully changed to working)

--- wordpress/wp-content/themes/Lora/inc/mail.php
+++ wordpress/wp-content/themes/Lora/inc/mail.php
@@ -1,50 +1,20 @@
 <?php
 require_once( '../../../../wp-load.php' );
-if( isset($_POST['submit'] ) ) {
-	
-	//Check to make sure that the name field is not empty
-	if( trim( $_POST['name'] ) === '' ) {
-		$name_error = 'You forgot to enter your name111.';
-		$error = true;
-	} else {
-		$name = trim( $_POST['name'] );
-	}
-	
-	//Check to make sure sure that a valid email address is submitted
-	if( trim( $_POST['email'] ) === '' )  {
-		$email_error = 'You forgot to enter your email address.';
-		$error = true;
-	} else if ( !eregi( "^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", trim( $_POST['email'] ) ) ) {
-		$email_error = 'You entered an invalid email address.';
-		$error = true;
-	} else {
-		$email = trim( $_POST['email'] );
-	}
-	
-	$subject = trim( $_POST['subject'] );
+if( isset($_GET['submit'] ) ) {
 
-	//Check to make sure comments were entered	
-	if( trim( $_POST['message'] ) === '' ) {
-		$message_error = 'You forgot to enter your comments.';
-		$error = true;
-	} else {
-		if( function_exists( 'stripslashes' ) ) {
-			$message = stripslashes( trim( $_POST['message'] ) );
-		} else {
-			$message = trim( $_POST['message'] );
-		}
-	}
-		
-	//If there is no error, send the email
-	if( !isset( $error ) ) {
-		$to = get_option( 'admin_email' );
-        echo 'qweqwe';
-		$subject = 'Contact Form Submission from ' . $name;
-		$body = "Name: $name \n\nEmail: $email \n\nComments: $message";
-		$headers = 'From: My Site <' . $to . '>' . "\r\n" . 'Reply-To: ' . $email;
-		
-		mail( $to, $subject, $body, $headers );
+    $name = trim($_GET['name']);
+    $email = trim($_GET['email']);
 
-		//$sent = true;
-	}
+    if( function_exists( 'stripslashes' ) ) {
+        $message = stripslashes( trim( $_GET['message'] ) );
+    } else {
+        $message = trim( $_GET['message'] );
+    }
+		
+    $to = get_option( 'admin_email' );
+    $subject = 'Contact Form Submission from ' . $name;
+    $body = "Name: $name \n\nEmail: $email \n\nComments: $message";
+    $headers = 'From: My Site <' . $to . '>' . "\r\n" . 'Reply-To: ' . $email;
+
+    mail( $to, $subject, $body, $headers );
 }
--- wordpress/wp-content/themes/Lora/js/contact-form.js
+++ wordpress/wp-content/themes/Lora/js/contact-form.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function($) {
 		if( !hasError ) {
 
 			$.ajax({
-				type: "POST",
+				type: "GET",
 				url: $(this).attr('data-src'),
 				data: $(this).serialize(),
 				success: function() {
